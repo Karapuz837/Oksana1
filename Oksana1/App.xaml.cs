5d166a2c3850faa74993ddf415dfbd87e18ee8d1
232010 <232010@WINMAC5>
2022-11-19 16:24:03 +0300

    Добавьте файлы проекта.

---
+++ Oksana1/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Oksana1.databaes;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Oksana1
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static OksanaEntities db = new OksanaEntities();
+    }
+}
---
+++ Oksana1/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Oksana1.databaes;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Oksana1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ToyList.ItemsSource = App.db.Toy.ToList();
+        }
+        private void SortFiled_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Refresh();
+        }
+        public void Refresh()
+        {
+            IEnumerable<Toy> filter = App.db.Toy;
+        }
+
+        private void SearchField_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
